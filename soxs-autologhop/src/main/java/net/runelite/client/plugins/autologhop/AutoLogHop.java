4cb40e6d731f1738a8b37af6676b8177b36a6b9e
Zak <4021809+Soxs@users.noreply.github.com>
2021-08-02 22:07:42 +0100

    Update AutoLogHop.java

--- soxs-autologhop/src/main/java/net/runelite/client/plugins/autologhop/AutoLogHop.java
+++ soxs-autologhop/src/main/java/net/runelite/client/plugins/autologhop/AutoLogHop.java
@@ -3,12 +3,7 @@ package net.runelite.client.plugins.autologhop;
 
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.MenuAction;
-import net.runelite.api.Player;
-import net.runelite.api.SkullIcon;
-import net.runelite.api.Varbits;
+import net.runelite.api.*;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.PlayerSpawned;
 import net.runelite.api.widgets.Widget;
@@ -36,241 +31,227 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Extension
 @PluginDescriptor(
-	name = "AutoLogHop",
-	description = "Auto hops/logs out when another player is seen.",
-	tags = {"logout", "hop worlds", "auto log", "auto hop", "soxs"},
-	enabledByDefault = false,
-	hidden = false
+        name = "AutoLogHop",
+        description = "Auto hops/logs out when another player is seen.",
+        tags = {"logout", "hop worlds", "auto log", "auto hop", "soxs"},
+        enabledByDefault = false,
+        hidden = false
 )
 @Slf4j
 @SuppressWarnings("unused")
 @Singleton
-public class AutoLogHop extends Plugin
-{
-	@Inject
-	private Client client;
+public class AutoLogHop extends Plugin {
+    @Inject
+    private Client client;
 
-	@Inject
-	private PluginManager pluginManager;
+    @Inject
+    private PluginManager pluginManager;
 
-	@Inject
-	private OverlayManager overlayManager;
+    @Inject
+    private OverlayManager overlayManager;
 
-	@Inject
-	private AutoLogHopConfig config;
+    @Inject
+    private AutoLogHopConfig config;
 
-	@Inject
-	private EventBus eventBus;
+    @Inject
+    private EventBus eventBus;
 
-	@Inject
-	private ClientThread clientThread;
+    @Inject
+    private ClientThread clientThread;
 
-	@Inject
-	private WorldService worldService;
+    @Inject
+    private WorldService worldService;
 
-	@Inject
-	private ExecutorService executor;
+    @Inject
+    private ExecutorService executor;
 
-	@Provides
-	AutoLogHopConfig getConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(AutoLogHopConfig.class);
-	}
+    @Provides
+    AutoLogHopConfig getConfig(ConfigManager configManager) {
+        return configManager.getConfig(AutoLogHopConfig.class);
+    }
 
-	@Override
-	protected void startUp()
-	{
+    @Override
+    protected void startUp() {
 
-	}
+    }
 
-	@Override
-	protected void shutDown()
-	{
+    @Override
+    protected void shutDown() {
 
-	}
+    }
 
-	@Subscribe
-	public void onGameTick(GameTick event){
-		if (passedWildernessChecks()) {
-			if (nearPlayer()) {
-				handleAction();
-			}
-		}
-	}
+    @Subscribe
+    public void onGameTick(GameTick event) {
+        if (nearPlayer()) {
+            handleAction();
+        }
 
-	@Subscribe
-	public void onPlayerSpawned(PlayerSpawned event) {
-		if (event.getPlayer() == client.getLocalPlayer())
-			return;
+    }
 
-		if (isInWhitelist(event.getPlayer().getName()))
-			return;
+    @Subscribe
+    public void onPlayerSpawned(PlayerSpawned event) {
+        if (isPlayerBad(event.getPlayer()))
+            handleAction();
+    }
 
-		if (event.getPlayer().getCombatLevel() < config.minCombat())
-			return;
+    private boolean nearPlayer() {
+        List<Player> players = client.getPlayers();
+        for (Player p : players) {
+            if (!isPlayerBad(p))
+                continue;
+            return true;
+        }
+        return false;
+    }
 
-		if (config.skulledOnly() && !isPlayerSkulled(event.getPlayer()))
-			return;
+    private void handleAction() {
+        if (config.hop())
+            hopToWorld(getValidWorld());
+        else {
+            logout();
+        }
+    }
 
-		if (passedWildernessChecks()) {
-			handleAction();
-		}
-	}
+    private boolean passedWildernessChecks() {
+        return config.disableWildyChecks() || inWilderness();
+    }
 
-	private boolean passedWildernessChecks() {
-		return config.disableWildyChecks() || inWilderness();
-	}
+    private boolean isPlayerBad(Player player) {
+        if (player == client.getLocalPlayer())
+            return false;
 
-	private boolean isPlayerSkulled(Player player) {
-		if (player == null) {
-			return false;
-		}
+        if (isInWhitelist(player.getName()))
+            return false;
 
-		return player.getSkullIcon() == SkullIcon.SKULL;
-	}
+        if (player.getCombatLevel() < config.minCombat())
+            return false;
 
-	private void handleAction() {
-		if (config.hop())
-			hopToWorld(getValidWorld());
-		else {
-			logout();
-		}
-	}
+        if (config.skulledOnly() && !isPlayerSkulled(player))
+            return false;
 
-	private boolean nearPlayer() {
-		List<Player> players = client.getPlayers();
-		for (Player p : players)
-		{
-			if (p == client.getLocalPlayer() || isInWhitelist(p.getName()) || p.getCombatLevel() < config.minCombat()
-					|| (config.skulledOnly() && !isPlayerSkulled(p)))
-				continue;
+        if (!passedWildernessChecks())
+            return false;
 
-			return true;
-		}
-		return false;
-	}
+        return true;
+    }
 
-	private int getValidWorld() {
-		WorldResult result = worldService.getWorlds();
-		if (result == null)
-			return -1;
-		List<World> worlds = result.getWorlds();
-		Collections.shuffle(worlds);
-		for (World w : worlds)
-		{
-			if (client.getWorld() == w.getId())
-				continue;
+    private int getValidWorld() {
+        WorldResult result = worldService.getWorlds();
+        if (result == null)
+            return -1;
+        List<World> worlds = result.getWorlds();
+        Collections.shuffle(worlds);
+        for (World w : worlds) {
+            if (client.getWorld() == w.getId())
+                continue;
 
-			if (w.getTypes().contains(net.runelite.http.api.worlds.WorldType.HIGH_RISK) ||
-					w.getTypes().contains(net.runelite.http.api.worlds.WorldType.DEADMAN) ||
-					w.getTypes().contains(net.runelite.http.api.worlds.WorldType.PVP) ||
-					w.getTypes().contains(net.runelite.http.api.worlds.WorldType.SKILL_TOTAL) ||
-					w.getTypes().contains(net.runelite.http.api.worlds.WorldType.BOUNTY) ||
-					config.membersWorlds() != w.getTypes().contains(net.runelite.http.api.worlds.WorldType.MEMBERS))
-				continue;
-			return w.getId();
-		}
-		return -1;
-	}
+            if (w.getTypes().contains(net.runelite.http.api.worlds.WorldType.HIGH_RISK) ||
+                    w.getTypes().contains(net.runelite.http.api.worlds.WorldType.DEADMAN) ||
+                    w.getTypes().contains(net.runelite.http.api.worlds.WorldType.PVP) ||
+                    w.getTypes().contains(net.runelite.http.api.worlds.WorldType.SKILL_TOTAL) ||
+                    w.getTypes().contains(net.runelite.http.api.worlds.WorldType.BOUNTY) ||
+                    config.membersWorlds() != w.getTypes().contains(net.runelite.http.api.worlds.WorldType.MEMBERS))
+                continue;
+            return w.getId();
+        }
+        return -1;
+    }
 
-	private void hopToWorld(int worldId)
-	{
-		assert client.isClientThread();
+    private void hopToWorld(int worldId) {
+        assert client.isClientThread();
 
-		WorldResult worldResult = worldService.getWorlds();
-		// Don't try to hop if the world doesn't exist
-		World world = worldResult.findWorld(worldId);
-		if (world == null)
-		{
-			return;
-		}
+        WorldResult worldResult = worldService.getWorlds();
+        // Don't try to hop if the world doesn't exist
+        World world = worldResult.findWorld(worldId);
+        if (world == null) {
+            return;
+        }
 
-		final net.runelite.api.World rsWorld = client.createWorld();
-		rsWorld.setActivity(world.getActivity());
-		rsWorld.setAddress(world.getAddress());
-		rsWorld.setId(world.getId());
-		rsWorld.setPlayerCount(world.getPlayers());
-		rsWorld.setLocation(world.getLocation());
-		rsWorld.setTypes(WorldUtil.toWorldTypes(world.getTypes()));
+        final net.runelite.api.World rsWorld = client.createWorld();
+        rsWorld.setActivity(world.getActivity());
+        rsWorld.setAddress(world.getAddress());
+        rsWorld.setId(world.getId());
+        rsWorld.setPlayerCount(world.getPlayers());
+        rsWorld.setLocation(world.getLocation());
+        rsWorld.setTypes(WorldUtil.toWorldTypes(world.getTypes()));
 
-		if (client.getGameState() == GameState.LOGIN_SCREEN)
-		{
-			// on the login screen we can just change the world by ourselves
-			client.changeWorld(rsWorld);
-			return;
-		}
+        if (client.getGameState() == GameState.LOGIN_SCREEN) {
+            // on the login screen we can just change the world by ourselves
+            client.changeWorld(rsWorld);
+            return;
+        }
 
-		if (client.getWidget(WidgetInfo.WORLD_SWITCHER_LIST) == null)
-		{
-			client.openWorldHopper();
+        if (client.getWidget(WidgetInfo.WORLD_SWITCHER_LIST) == null) {
+            client.openWorldHopper();
 
-			executor.submit(() -> {
-				try {
-					Thread.sleep(25 + ThreadLocalRandom.current().nextInt(125));
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				injector.getInstance(ClientThread.class).invokeLater(() -> {
-					if (client.getWidget(WidgetInfo.WORLD_SWITCHER_LIST) != null)
-						client.hopToWorld(rsWorld);
-				});
-			});
-		} else {
-			client.hopToWorld(rsWorld);
-		}
+            executor.submit(() -> {
+                try {
+                    Thread.sleep(25 + ThreadLocalRandom.current().nextInt(125));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                injector.getInstance(ClientThread.class).invokeLater(() -> {
+                    if (client.getWidget(WidgetInfo.WORLD_SWITCHER_LIST) != null)
+                        client.hopToWorld(rsWorld);
+                });
+            });
+        } else {
+            client.hopToWorld(rsWorld);
+        }
 
 
-	}
+    }
 
-	private void logout() {
-		Widget logoutButton = client.getWidget(182, 8);
-		Widget logoutDoorButton = client.getWidget(69, 23);
-		int param1 = -1;
-		if (logoutButton != null)
-		{
-			param1 = logoutButton.getId();
-		}
-		else if (logoutDoorButton != null)
-		{
-			param1 = logoutDoorButton.getId();
-		}
-		if (param1 == -1)
-		{
-			return;
-		}
-		client.invokeMenuAction(
-				"Logout",
-				"",
-				1,
-				MenuAction.CC_OP.getId(),
-				-1,
-				param1
-		);
-	}
+    private void logout() {
+        Widget logoutButton = client.getWidget(182, 8);
+        Widget logoutDoorButton = client.getWidget(69, 23);
+        int param1 = -1;
+        if (logoutButton != null) {
+            param1 = logoutButton.getId();
+        } else if (logoutDoorButton != null) {
+            param1 = logoutDoorButton.getId();
+        }
+        if (param1 == -1) {
+            return;
+        }
+        client.invokeMenuAction(
+                "Logout",
+                "",
+                1,
+                MenuAction.CC_OP.getId(),
+                -1,
+                param1
+        );
+    }
 
-	public boolean inWilderness()
-	{
-		return client.getVar(Varbits.IN_WILDERNESS) == 1;
-	}
+    public boolean inWilderness() {
+        return client.getVar(Varbits.IN_WILDERNESS) == 1;
+    }
 
-	public boolean isInWhitelist(String username)
-	{
-		username = username.toLowerCase().replace(" ", "_");
-		String[] names = config.whitelist().toLowerCase().replace(" ", "_").split(",");
+    public boolean isInWhitelist(String username) {
+        username = username.toLowerCase().replace(" ", "_");
+        String[] names = config.whitelist().toLowerCase().replace(" ", "_").split(",");
 
-		for (String whitelisted : names)
-		{
-			if (whitelisted.isBlank() || whitelisted.isEmpty() || whitelisted.equals("_"))
-				continue;
+        for (String whitelisted : names) {
+            if (whitelisted.isBlank() || whitelisted.isEmpty() || whitelisted.equals("_"))
+                continue;
 
-			//remove trailing whitespace on names.
-			//if (whitelisted.charAt(whitelisted.length() - 1) == ' ')
-			//	whitelisted = whitelisted.substring(0, whitelisted.length() - 1);
+            //remove trailing whitespace on names.
+            //if (whitelisted.charAt(whitelisted.length() - 1) == ' ')
+            //	whitelisted = whitelisted.substring(0, whitelisted.length() - 1);
 
-			if (whitelisted.equals(username))
-				return true;
-		}
-		return false;
-	}
+            if (whitelisted.equals(username))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean isPlayerSkulled(Player player) {
+        if (player == null) {
+            return false;
+        }
+
+        return player.getSkullIcon() == SkullIcon.SKULL;
+    }
 
 }
